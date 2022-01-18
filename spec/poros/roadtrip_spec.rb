614d9d1eb74fe4a69b97c14065d0b78151e4d2a2
Philip Medlin <phmedlin@gmail.com>
2022-01-17 18:54:03 -0700

    Roadtrip, session, user request tests

--- spec/poros/roadtrip_spec.rb
+++ spec/poros/roadtrip_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Roadtrip, type: :model do
-
-  describe 'relationships' do
-    it { should belong_to :user }
+describe Roadtrip do
+  before :each do
+    data = {
+            :start_city=>"Los Angeles, CA",
+            :end_city=>"New York City, NY",
+            :travel_time=>"40:21:05",
+            :weather_at_eta=>
+              {:temperature=>38.21,
+               :conditions=>"overcast clouds"}
+             }
+    @roadtrip = Roadtrip.new(data)
   end
 
+  it 'exists' do
+    expect(@roadtrip).to be_an_instance_of(Roadtrip)
+  end
+
+  it 'attributes' do
+      expect(@roadtrip.start_city).to eq("Los Angeles, CA")
+      expect(@roadtrip.end_city).to eq("New York City, NY")
+      expect(@roadtrip.travel_time).to eq("40:21:05")
+      expect(@roadtrip.weather_at_eta).to be_a Hash
+  end
 end
--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -1,7 +1,28 @@
-# require 'rails_helper'
-#
-# describe 'Sessions API' do
-#   it '...' do
-#
-#   end
-# end
+require 'rails_helper'
+
+describe 'Sessions API' do
+  it 'can log in an existing user' do
+    json_payload = {
+                    "email": "whatever@example.com",
+                    "password": "password",
+                    "password_confirmation": "password"
+                    }
+    post '/api/v1/users', params: json_payload, as: :json
+
+    json_payload = {
+                    "email": "whatever@example.com",
+                    "password": "password"
+                    }
+    post '/api/v1/sessions', params: json_payload, as: :json
+
+    response_hash = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response_hash).to have_key(:id)
+    expect(response_hash[:id].to_i).to be > 0
+    expect(response_hash[:attributes][:email]).to eq("whatever@example.com")
+    expect(response_hash[:attributes]).not_to have_key(:password)
+    expect(response_hash[:attributes]).not_to have_key(:password_confirmation)
+    expect(response_hash[:attributes]).to have_key(:api_key)
+    expect(response_hash[:attributes][:api_key].split('').count).to eq 32
+  end
+end
